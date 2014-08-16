d22138cf38bd1402f10822cd83ad13f3353da1f7
Hakan Lindestaf <hakan@lindestaf.com>
2014-08-15 17:04:59 -0700

    Created NuGet packages for logging

--- BuildNuGet.bat
+++ BuildNuGet.bat
@@ -1,3 +1,11 @@
+PUSHD Logging
+C:\Temp\nuget.exe pack Logging.csproj -IncludeReferencedProjects -Prop Configuration=Release -OutputDirectory ..\_NuGetOutput\
+POPD
+
+PUSHD Logging.NLog
+C:\Temp\nuget.exe pack Logging.NLog.csproj -IncludeReferencedProjects -Prop Configuration=Release -OutputDirectory ..\_NuGetOutput\
+POPD
+
 PUSHD Qlue.Core
 C:\Temp\nuget.exe pack Qlue.Core.csproj -IncludeReferencedProjects -Prop Configuration=Release -OutputDirectory ..\_NuGetOutput\
 POPD
