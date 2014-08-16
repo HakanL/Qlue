5cc4cdbd9884d1e2f7c93dc92c94cff691cbd6f6
Hakan Lindestaf <hakan@lindestaf.com>
2014-08-15 17:11:50 -0700

    Removed Server

--- BuildNuGet.bat
+++ BuildNuGet.bat
@@ -13,7 +13,3 @@ POPD
 PUSHD Qlue.Client
 C:\Temp\nuget.exe pack Qlue.Client.csproj -IncludeReferencedProjects -Prop Configuration=Release -OutputDirectory ..\_NuGetOutput\
 POPD
-
-PUSHD Qlue.Server
-C:\Temp\nuget.exe pack Qlue.Server.csproj -IncludeReferencedProjects -Prop Configuration=Release -OutputDirectory ..\_NuGetOutput\
-POPD
