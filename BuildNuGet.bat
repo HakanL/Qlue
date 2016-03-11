6ec1b160a143fcbe91243295affcb35c9bb959f0
Hakan Lindestaf <hakan@lindestaf.com>
2016-03-11 15:38:42 -0800

    Added Serilog to NuGet build file

--- BuildNuGet.bat
+++ BuildNuGet.bat
@@ -6,6 +6,10 @@ PUSHD Logging.NLog
 C:\Temp\nuget.exe pack Logging.NLog.csproj -IncludeReferencedProjects -Prop Configuration=Release -OutputDirectory ..\_NuGetOutput\
 POPD
 
+PUSHD Logging.Serilog
+C:\Temp\nuget.exe pack Logging.Serilog.csproj -IncludeReferencedProjects -Prop Configuration=Release -OutputDirectory ..\_NuGetOutput\
+POPD
+
 PUSHD Qlue.Core
 C:\Temp\nuget.exe pack Qlue.Core.csproj -IncludeReferencedProjects -Prop Configuration=Release -OutputDirectory ..\_NuGetOutput\
 POPD
